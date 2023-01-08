772e8f20af76057509684b1f9b3ff355c6b7eb4f
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2023-01-08 15:12:59 -0800

    added alt tag to header.js

--- components/Header/header.js
+++ components/Header/header.js
@@ -3,7 +3,7 @@ export default function Header(){
     const position = [49.28594, -123.11129]
     return(
     <div className="text-3xl underline w-full bg-offWhite flex justify-center p-3">
-        <img className="w-80" src="frogLink.png"></img>
+        <img alt="froglink logo" className="w-80" src="frogLink.png"></img>
     </div>
     )
 }
